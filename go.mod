546801572c9825ecf517a0e2b249ba0c383463d0
Adi Putra <adiputrapermana@gmail.com>
2022-09-02 03:12:31 +0700

    initialize say hello app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AdiPP/app-say-hello
+
+go 1.18
+
+require github.com/AdiPP/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/AdiPP/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
